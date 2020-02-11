56c217b4b88f5a286921a611786f0a0613d936a1
User <dbrajs@gmail.com>
2020-02-11 16:04:54 +0100

    one

--- src/models/user.py
+++ src/models/user.py
@@ -1,23 +1,40 @@
-
-
+import uuid
+from src.common.database import Database
 
 
 class User(object):
     def __init__(self, email, password):
         self.email = email
         self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-    def get_by_email(self):
-        Database.find_one("user", email)
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one("users", {"email": self.email})
+        if data is not None:
+            return cls(**data)
+    @classmethod
+    def get_by_id(cls, _id):
+         data = Database.find_one("users", {"_id": _id})
+         if data is not None:
+             return cls(**data)
 
-    def get_by_id(self):
-        pass
+    @staticmethod
+    def valid_login(self, email, password):
+        user = user.get_by_email(email)
+        if user is not None:
+            return user.password == password
+        return False
 
-    def valid_login(self):
-        pass
-
-    def register(self):
-        pass
+    @sclassmethod
+    def register(cls, email, password):
+        user = user.get_by_email(email)
+        if user is None:
+            new_user = User(email,password)
+            new_user.saveToMongo()
+            return True
+        else:
+            return False
 
     def login(self):
         pass
